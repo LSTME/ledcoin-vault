c1c7920cf8a2d8d0cb6c73a4477bd6edda8fd13e
Valter Martinek <martinek@freevision.sk>
2017-08-15 00:40:23 +0200

    Add user detail view.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/:id', (req, res, next) => {
+  const { dataSource } = req;
+  const user = dataSource.getUser(req.params.id)[0];
+  res.render('user', { user });
+});
+
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   const { dataSource } = req;
