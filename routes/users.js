11c022c1771917b744a9237b5e93631e37e0325f
Valter Martinek <martinek@freevision.sk>
2017-08-16 13:55:45 +0200

    Users now can be all removed.

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/:id', (req, res, next) => {
-  const { dataSource } = req;
-  const user = dataSource.getUser(req.params.id)[0];
-  res.render('users/show', { user });
-});
-
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   const { dataSource } = req;
   const users = dataSource.getUsers();
   res.render('users/index', { users });
+});
+
+router.get('/clearConfirm', (req, res, next) => {
+  res.render('users/clearConfirm');
+});
+
+router.post('/clear', (req, res) => {
+  req.dataSource.clearUsers();
+  res.redirect('/users');
+});
+
+router.get('/:id', (req, res, next) => {
+  const { dataSource } = req;
+  const user = dataSource.getUser(req.params.id)[0];
+  res.render('users/show', { user });
 });
 
 module.exports = router;
