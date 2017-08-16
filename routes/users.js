0586c293227e1c3d459bebe982623966d2f8cc14
Valter Martinek <martinek@freevision.sk>
2017-08-16 14:47:20 +0200

    Add walletId to user form.

--- routes/users.js
+++ routes/users.js
@@ -21,18 +21,19 @@ router.post('/clear', (req, res) => {
 router.get('/:id/edit', (req, res) => {
   const { dataSource } = req;
   const user = dataSource.getUser(req.params.id)[0];
-  res.render('users/edit', { result: { value: user } });
+  res.render('users/edit', { user });
 });
 
 router.post('/:id', (req, res) => {
   const ds = req.dataSource;
 
-  const changes = _.pick(req.body, ['firstName', 'lastName', 'photo', 'dateOfBirth']);
+  const changes = _.pick(req.body, ['firstName', 'lastName', 'photo', 'dateOfBirth', 'walletId']);
   const result = ds.schema.validate(changes, ds.schema.user);
   console.log(result);
 
   if (result.error) {
-    res.render('users/edit', { result });
+    const user = ds.getUser(req.params.id)[0];
+    res.render('users/edit', { user, result });
   } else {
     const user = ds.getUser(req.params.id)[0];
     ds.saveUser(_.merge(user, changes));
