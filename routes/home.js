bdef11550fbe850a56e18a0c492cb72bca5399a1
Valter Martinek <martinek@freevision.sk>
2017-08-16 19:26:42 +0200

    Removed express home.

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('home/index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('home/index');
 });
 
 module.exports = router;
