24941889000e9fa2554611519faa10106d752b62
Valter Martinek <martinek@freevision.sk>
2017-08-16 14:33:46 +0200

    Users now can be edited.

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const _ = require('lodash');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   const { dataSource } = req;
   const users = dataSource.getUsers();
   res.render('users/index', { users });
 });
 
-router.get('/clearConfirm', (req, res, next) => {
+router.get('/clearConfirm', (req, res) => {
   res.render('users/clearConfirm');
 });
 
@@ -18,7 +18,29 @@ router.post('/clear', (req, res) => {
   res.redirect('/users');
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id/edit', (req, res) => {
+  const { dataSource } = req;
+  const user = dataSource.getUser(req.params.id)[0];
+  res.render('users/edit', { result: { value: user } });
+});
+
+router.post('/:id', (req, res) => {
+  const ds = req.dataSource;
+
+  const changes = _.pick(req.body, ['firstName', 'lastName', 'photo', 'dateOfBirth']);
+  const result = ds.schema.validate(changes, ds.schema.user);
+  console.log(result);
+
+  if (result.error) {
+    res.render('users/edit', { result });
+  } else {
+    const user = ds.getUser(req.params.id)[0];
+    ds.saveUser(_.merge(user, changes));
+    res.redirect('/users');
+  }
+});
+
+router.get('/:id', (req, res) => {
   const { dataSource } = req;
   const user = dataSource.getUser(req.params.id)[0];
   res.render('users/show', { user });
