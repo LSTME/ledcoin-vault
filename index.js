8e9c84c0e437b15d2ff82234296d357105d6c5a5
Valter Martinek <martinek@freevision.sk>
2017-08-17 21:04:51 +0200

    Web and Tcp now share context.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const logger = require('./lib/logger');
 const web = require('./lib/web');
 const tcp = require('./lib/tcp');
 
+const context = {};
+
 Promise.resolve()
-  .then(async () => web(process.env.NODE_ENV || 'development'))
-  .then(async () => tcp(process.env.NODE_ENV || 'development'))
+  .then(async () => web(process.env.NODE_ENV || 'development', context))
+  .then(async () => tcp(process.env.NODE_ENV || 'development', context))
   .catch(error => logger.error(error));
 
