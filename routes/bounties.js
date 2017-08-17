529f39ff84d001e94b014327522d7839f9f33785
Valter Martinek <martinek@freevision.sk>
2017-08-17 16:12:30 +0200

    Add edit for bounties. Value is readonly.

--- routes/bounties.js
+++ routes/bounties.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 
 const router = express.Router();
 
@@ -8,17 +9,17 @@ const router = express.Router();
 //   res.render('bounty', { bounty });
 // });
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   const { dataSource } = req;
   const bounties = dataSource.getBounties();
   res.render('bounties/index', { bounties });
 });
 
-router.get('/new', (req, res, next) => {
+router.get('/new', (req, res) => {
   res.render('bounties/new');
 });
 
-router.post('/create', (req, res, next) => {
+router.post('/create', (req, res) => {
   const ds = req.dataSource;
   const result = ds.schema.validate(req.body, ds.schema.bounty);
   console.log(result);
@@ -39,10 +40,29 @@ router.get('/secret/:key', (req, res) => {
   res.render('bounties/secret', { bounty, bountyCodeData });
 });
 
+router.get('/:id/edit', (req, res) => {
+  const { dataSource } = req;
+  const bounty = dataSource.getBounty(req.params.id);
+  res.render('bounties/edit', { bounty });
+});
+
+router.post('/:id', (req, res) => {
+  const ds = req.dataSource;
+  const bounty = ds.getBounty(req.params.id);
+  const changes = _.pick(req.body, ['description', 'value', 'code', 'target', 'urlKey']);
+  const result = ds.schema.validate(changes, ds.schema.bounty);
+
+  if (result.error) {
+    res.render('bounties/edit', { bounty, result });
+  } else {
+    ds.saveUser(_.merge(bounty, changes));
+    res.redirect('/bounties');
+  }
+});
+
 router.get('/:id', (req, res) => {
   const { dataSource } = req;
   const bounty = dataSource.getBounty(req.params.id);
-  console.log(req.hostname);
   bounty.url = bounty.urlKey ? `/bounties/secret/${bounty.urlKey}` : undefined;
   res.render('bounties/show', { bounty });
 });
