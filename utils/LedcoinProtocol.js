db5bd775dca6d70386d8b14012608ef3a42f45a4
Valter Martinek <martinek@freevision.sk>
2017-08-18 12:01:58 +0200

    Refactored LedcoinProtocol.js to function.

--- utils/LedcoinProtocol.js
+++ utils/LedcoinProtocol.js
@@ -1,113 +1,36 @@
-/* eslint no-bitwise: 0 */
-const CRC8 = require('./crc8');
+/* eslint no-bitwise: 0, func-names: 0 */
+const crc = require('./crc8');
+const crypto = require('./crypto');
 
-const crc = new CRC8();
+const BYTE = 0xFF;
 
-const COMMANDS = {
-  GetAuth: 1,
-  SetAuth: 2,
-  GetBalance: 3,
-  SetBalance: 4,
-  GetLog: 5,
-  ShrinkLog: 6,
-  SetReadyToGame: 7,
-  SetGamePacket: 8,
-  GetEEPROM: 14,
-  WriteEEPROM: 15,
-  SetBounty: 16,
-};
+function LedcoinProtocol() {
+  this.Commands = {
+    GetAuth: 1,
+    SetAuth: 2,
+    GetBalance: 3,
+    SetBalance: 4,
+    GetLog: 5,
+    ShrinkLog: 6,
+    SetReadyToGame: 7,
+    SetGamePacket: 8,
+    GetEEPROM: 14,
+    WriteEEPROM: 15,
+    SetBounty: 16,
+  };
 
-const BalanceCounter = {
-  Main: 0,
-  Bounty: 1,
-  Games: 2,
-};
+  this.BalanceCounter = {
+    Main: 0,
+    Bounty: 1,
+    Games: 2,
+  };
 
-const OperationType = {
-  AddLedcoin: 0,
-  DeduceLedcoin: 1,
-};
+  this.OperationType = {
+    AddLedcoin: 0,
+    DeduceLedcoin: 1,
+  };
 
-class LedcoinProtocol {
-  static GetAuth(callerId, targetChallenge) {
-    const body = new Uint8ClampedArray(3);
-    body[0] = callerId;
-    body[1] = (targetChallenge >> 8) & 255;
-    body[2] = targetChallenge & 255;
-    return LedcoinProtocol.BuildRequest(COMMANDS.GetAuth, body);
-  }
-
-  static SetAuth(callerId, callerSignature) {
-    const body = new Uint8ClampedArray(2);
-    body[0] = callerId;
-    body[1] = (callerSignature >> 8) & 255;
-    body[2] = callerSignature & 255;
-    return LedcoinProtocol.BuildRequest(COMMANDS.SetAuth, body);
-  }
-
-  static GetBalance(counterId) {
-    const body = new Uint8ClampedArray([counterId]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.GetBalance, body);
-  }
-
-  static SetBalance(operationType, value) {
-    const body = new Uint8ClampedArray([operationType, value]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.SetBalance, body);
-  }
-
-  static GetLog(dataId) {
-    const body = new Uint8ClampedArray([dataId]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.GetLog, body);
-  }
-
-  static ShrinkLog(dataId, keepCount) {
-    const body = new Uint8ClampedArray([dataId, keepCount]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.ShrinkLog, body);
-  }
-
-  static SetReadyToGame(gameId) {
-    const body = new Uint8ClampedArray([gameId]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.SetReadyToGame, body);
-  }
-
-  static SetGamePacket(gameId, packet) {
-    const body = new Uint8ClampedArray([gameId].concat(packet));
-    return LedcoinProtocol.BuildRequest(COMMANDS.SetGamePacket, body);
-  }
-
-  static GetEEPROM(address, length) {
-    const body = new Uint8ClampedArray([
-      (address >> 8) & 255,
-      address & 255,
-      (length >> 8) & 255,
-      length & 255,
-    ]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.GetEEPROM, body);
-  }
-
-  static WriteEEPROM(address, data) {
-    const body = new Uint8ClampedArray(2 + data.length);
-    body[0] = (address >> 8) & 255;
-    body[1] = address & 255;
-    data.forEach((d, i) => {
-      body[i + 2] = d;
-    });
-    return LedcoinProtocol.BuildRequest(COMMANDS.WriteEEPROM, body);
-  }
-
-  static SetBounty(bountyId, recipient, value, signature) {
-    const body = new Uint8ClampedArray([
-      (bountyId >> 8) & 255,
-      bountyId & 255,
-      recipient,
-      value,
-      (signature >> 8) & 255,
-      signature & 255,
-    ]);
-    return LedcoinProtocol.BuildRequest(COMMANDS.SetBounty, body);
-  }
-
-  static BuildRequest(commandId, body) {
+  this.BuildRequest = (commandId, body) => {
     if (body.constructor.name !== 'Uint8ClampedArray') {
       throw new Error(`Wrong body type: ${body.constructor.name}`);
     }
@@ -115,22 +38,101 @@ class LedcoinProtocol {
     if (body.length > 8) {
       throw new Error(`Body length out of range: ${body.length}`);
     }
+
     // 1 x CCCC CLLL - header: command_id + body length (1 byte)
     // 8 x BBBB BBBB - body (1 ~ 8 bytes)
     // 1 x RRRR RRRR - CRC (1 byte)
     const res = new Uint8ClampedArray(body.length + 2);
 
-    res[0] = (commandId << 3) | body.length;
-    body.forEach((v, i) => {
-      res[i + 1] = v;
-    });
-    res[res.length - 1] = crc.checksum(res);
+    res[0] = (commandId << 3) | (body.length - 1);
+    body.forEach((v, i) => { res[i + 1] = v; });
+    res[res.length - 1] = crc.checksum(res.slice(0, -1));
 
     return res;
-  }
+  };
 }
 
-const LP = LedcoinProtocol;
+LedcoinProtocol.prototype.GetAuth = function (callerId, targetChallenge) {
+  const body = new Uint8ClampedArray([
+    callerId,
+    BYTE & (targetChallenge >> 8),
+    BYTE & targetChallenge,
+  ]);
+  return this.BuildRequest(this.Commands.GetAuth, body);
+};
+
+LedcoinProtocol.prototype.SetAuth = function (callerId, callerSignature) {
+  const body = new Uint8ClampedArray([
+    callerId,
+    BYTE & (callerSignature >> 8),
+    BYTE & callerSignature,
+  ]);
+  return this.BuildRequest(this.Commands.SetAuth, body);
+};
+
+LedcoinProtocol.prototype.GetBalance = function (counterId) {
+  const body = new Uint8ClampedArray([counterId]);
+  return this.BuildRequest(this.Commands.GetBalance, body);
+};
+
+LedcoinProtocol.prototype.SetBalance = function (operationType, value) {
+  const body = new Uint8ClampedArray([operationType, value]);
+  return this.BuildRequest(this.Commands.SetBalance, body);
+};
+
+LedcoinProtocol.prototype.GetLog = function (dataId) {
+  const body = new Uint8ClampedArray([dataId]);
+  return this.BuildRequest(this.Commands.GetLog, body);
+};
+
+LedcoinProtocol.prototype.ShrinkLog = function (dataId, keepCount) {
+  const body = new Uint8ClampedArray([dataId, keepCount]);
+  return this.BuildRequest(this.Commands.ShrinkLog, body);
+};
+
+LedcoinProtocol.prototype.SetReadyToGame = function (gameId) {
+  const body = new Uint8ClampedArray([gameId]);
+  return this.BuildRequest(this.Commands.SetReadyToGame, body);
+};
+
+LedcoinProtocol.prototype.SetGamePacket = function (gameId, packet) {
+  const body = new Uint8ClampedArray([gameId].concat(packet));
+  return this.BuildRequest(this.Commands.SetGamePacket, body);
+};
+
+LedcoinProtocol.prototype.GetEEPROM = function (address, length) {
+  const body = new Uint8ClampedArray([
+    BYTE & (address >> 8),
+    BYTE & address,
+    BYTE & (length >> 8),
+    BYTE & length,
+  ]);
+  return this.BuildRequest(this.Commands.GetEEPROM, body);
+};
+
+LedcoinProtocol.prototype.WriteEEPROM = function (address, data) {
+  const body = new Uint8ClampedArray(2 + data.length);
+  body[0] = BYTE & (address >> 8);
+  body[1] = BYTE & address;
+  data.forEach((d, i) => {
+    body[i + 2] = d;
+  });
+  return this.BuildRequest(this.Commands.WriteEEPROM, body);
+};
+
+LedcoinProtocol.prototype.SetBounty = function (bountyId, recipient, value, signature) {
+  const body = new Uint8ClampedArray([
+    BYTE & (bountyId >> 8),
+    BYTE & bountyId,
+    BYTE & recipient,
+    BYTE & value,
+    BYTE & (signature >> 8),
+    BYTE & signature,
+  ]);
+  return this.BuildRequest(this.Commands.SetBounty, body);
+};
+
+const LP = new LedcoinProtocol();
 
 const sampleCallerId = 0xFF;
 const sampleTargetChallenge = 0xFFFF;
@@ -146,12 +148,12 @@ console.log(
   LP.SetAuth(sampleCallerId, sampleCallerSignature),
 );
 
-console.log('GetBalance(Main):', LP.GetBalance(BalanceCounter.Main));
-console.log('GetBalance(Bounty):', LP.GetBalance(BalanceCounter.Bounty));
-console.log('GetBalance(Games):', LP.GetBalance(BalanceCounter.Games));
+console.log('GetBalance(Main):', LP.GetBalance(LP.BalanceCounter.Main));
+console.log('GetBalance(Bounty):', LP.GetBalance(LP.BalanceCounter.Bounty));
+console.log('GetBalance(Games):', LP.GetBalance(LP.BalanceCounter.Games));
 
-console.log('SetBalance(AddLedcoin, 255):', LP.SetBalance(OperationType.AddLedcoin, 255));
-console.log('SetBalance(DeduceLedcoin, 255):', LP.SetBalance(OperationType.DeduceLedcoin, 255));
+console.log('SetBalance(AddLedcoin, 255):', LP.SetBalance(LP.OperationType.AddLedcoin, 255));
+console.log('SetBalance(DeduceLedcoin, 255):', LP.SetBalance(LP.OperationType.DeduceLedcoin, 255));
 
 console.log('GetLog(255):', LP.GetLog(255));
 
@@ -181,5 +183,8 @@ const sampleRecipient = 0xF3;
 const sampleValue = 0xF4;
 const sampleSignature = 0xF5F6;
 console.log(`SetBounty(${sampleBountyId}, ${sampleRecipient}, ${sampleValue}, ${sampleSignature})`, LP.SetBounty(sampleBountyId, sampleRecipient, sampleValue, sampleSignature));
+
+const sig = crypto.M(255, 3, 1);
+console.log(`SetBounty(1, 255, 3, ${sig})`, LP.SetBounty(1, 255, 3, sig), 'should be', [133, 0, 1, 255, 3, 39, 125, 111]);
 
 module.exports = LedcoinProtocol;
