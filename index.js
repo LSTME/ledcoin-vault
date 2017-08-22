fabf485545c2db7d8aa3cd37b170f565c4cf55bc
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2017-08-23 00:16:33 +0200

    Fixed database access to have single instance

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const logger = require('./lib/logger');
 const web = require('./lib/web');
 const tcp = require('./lib/tcp');
 
-const context = {};
+const configLoader = require('./lib/configLoader');
+const dbLoader = require('./lib/dbLoader');
+
+let context = {};
 
 Promise.resolve()
-  .then(async () => web(process.env.NODE_ENV || 'development', context))
-  .then(async () => tcp(process.env.NODE_ENV || 'development', context))
+  .then(async () => {
+    const environment = process.env.NODE_ENV || 'development';
+    const config = await configLoader(environment);
+    const db = await dbLoader(config.db.url);
+
+    context = {
+      environment,
+      config,
+      db,
+    };
+  })
+  .then(async () => web(context.environment, context))
+  .then(async () => tcp(context.environment, context))
   .catch(error => logger.error(error));
 
