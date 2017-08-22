c8c5575329ce4cb990ac69ba1d9f5338f9497b7a
Valter Martinek <martinek@freevision.sk>
2017-08-22 19:23:35 +0200

    New user now can be added.

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,41 @@ router.post('/clear', (req, res) => {
   res.redirect('/users');
 });
 
+router.get('/new', (req, res) => {
+  res.render('users/new');
+});
+
+router.post('/', (req, res) => {
+  const ds = req.dataSource;
+  const changes = _.pick(req.body, ['firstName', 'lastName', 'username', 'photo', 'dateOfBirth', 'walletId']);
+  const result = ds.schema.validate(changes, ds.schema.user);
+
+  changes.walletId = Number(changes.walletId);
+  changes.admin = Number(req.body.admin === 'on');
+
+  // make sure username is uniq
+  const isUsernameUniq = ds.getUsers({ username: changes.username }).length === 0;
+  if (!isUsernameUniq) {
+    ds.addJoiError(result, 'username', 'any.uniq', 'must be uniq');
+  }
+
+  // when creating user, password is required
+  if (req.body.password.length < 5) {
+    ds.addJoiError(result, 'password', 'any.length', 'must have at least 5 characters');
+  }
+
+  changes.password = ds.pHash(req.body.password);
+
+  if (result.error) {
+    res.render('users/new', { result });
+  } else {
+    changes.createdAt = new Date();
+    changes.updatedAt = new Date();
+    ds.createUsers([changes]);
+    res.redirect('/users');
+  }
+});
+
 router.get('/:id/edit', (req, res) => {
   const { dataSource } = req;
   const user = dataSource.getUser(req.params.id);
